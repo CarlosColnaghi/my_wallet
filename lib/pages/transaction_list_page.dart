3b606262e6fadfa33a6bda592d5e721529fcb60c
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2026-08-13 22:57:22 -0300

    wip: starting to create the transaction list

--- lib/pages/transaction_list_page.dart
+++ lib/pages/transaction_list_page.dart
@@ -9,7 +9,33 @@ class TransactionListPageState extends State<TransactionListPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Transações'))
+      appBar: AppBar(
+        title: Text(
+          "Transações",
+          style: TextStyle(
+            color: Colors.white
+          )
+        ),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: ListView.builder(itemBuilder: (BuildContext context, int i) {
+        if (i < 10) {
+          return Card(
+            elevation: 2.0,
+            child: ListTile(
+              leading: i%2 == 0 ? Icon(Icons.arrow_circle_up_rounded, size: 35, color: Colors.green,) : Icon(Icons.arrow_circle_down, size: 35, color: Colors.red,),
+              title: Text("Lorem Ipsum"),
+              subtitle: Text("Lorem Ipsum"),
+              trailing: i%2 == 0 ? Text("R\$10.00", style: TextStyle(fontSize: 20)) : Text("- R\$10.00", style: TextStyle(fontSize: 20),),
+            )
+          );
+        }
+      }),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+      },
+      backgroundColor: Colors.greenAccent,
+      foregroundColor: Colors.white,
+      child: Icon(Icons.add),),
     );
   }
 }
