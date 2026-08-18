91e87692c0ddfaa16d5a59a545616c9029c03448
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2026-08-17 23:38:21 -0300

    wip: creating base layout for transaction form page

--- lib/pages/transaction_form_page.dart
+++ lib/pages/transaction_form_page.dart
@@ -6,17 +6,60 @@ class TransactionFormPage extends StatefulWidget {
 }
 
 class TransactionFormState extends State<TransactionFormPage>{
+  InputDecoration _inputDecoration(String labelText, String hintText){
+    return InputDecoration(
+      labelText: labelText,
+      labelStyle: TextStyle(color: Colors.black),
+      floatingLabelStyle: TextStyle(color: Colors.greenAccent, fontWeight: FontWeight.bold, fontSize: 20),
+      border: OutlineInputBorder(),
+      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.greenAccent, width: 2)),
+      hintText: hintText,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-            "Nova Transação",
-            style: TextStyle(
-                color: Colors.white
-            )
+          'Nova Transação',
+          style: TextStyle(
+            color: Colors.white
+          )
         ),
         backgroundColor: Colors.greenAccent,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              decoration: _inputDecoration('Título', 'Ex.: Aluguel, Água, Luz, Gás e etc.'),
+            ),
+            SizedBox(height: 10,),
+            TextField(
+                decoration: _inputDecoration('Descrição', 'Ex.: Aluguel referente ao mês de agosto')
+            ),
+            SizedBox(height: 10,),
+            TextField(
+              decoration: _inputDecoration('Valor', 'R\$ 0,00')
+            ),
+            SizedBox(height: 30,),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent, foregroundColor: Colors.white),
+              child: Text('Salvar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent, foregroundColor: Colors.white),
+              child: Text('Cancelar'),
+            )
+          ],
+        )
       ),
     );
   }
--- lib/pages/transaction_list_page.dart
+++ lib/pages/transaction_list_page.dart
@@ -12,7 +12,7 @@ class TransactionListState extends State<TransactionListPage>{
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Transações",
+          'Transações',
           style: TextStyle(
             color: Colors.white
           )
@@ -25,9 +25,9 @@ class TransactionListState extends State<TransactionListPage>{
             elevation: 2.0,
             child: ListTile(
               leading: i%2 == 0 ? Icon(Icons.arrow_circle_up_rounded, size: 35, color: Colors.green,) : Icon(Icons.arrow_circle_down, size: 35, color: Colors.red,),
-              title: Text("Lorem Ipsum"),
-              subtitle: Text("Lorem Ipsum"),
-              trailing: i%2 == 0 ? Text("R\$10.00", style: TextStyle(fontSize: 20)) : Text("- R\$10.00", style: TextStyle(fontSize: 20),),
+              title: Text('Lorem Ipsum'),
+              subtitle: Text('Lorem Ipsum'),
+              trailing: i%2 == 0 ? Text('R\$10.00', style: TextStyle(fontSize: 20)) : Text('- R\$10.00', style: TextStyle(fontSize: 20),),
             )
           );
         }
