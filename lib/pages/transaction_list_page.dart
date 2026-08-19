89250e8ae6d842cc9aeb622e2b8d88a07f945464
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2026-08-18 22:14:12 -0300

    feat: displaying total value

--- lib/pages/transaction_list_page.dart
+++ lib/pages/transaction_list_page.dart
@@ -19,19 +19,32 @@ class TransactionListState extends State<TransactionListPage>{
         ),
         backgroundColor: Colors.greenAccent,
       ),
-      body: ListView.builder(itemBuilder: (BuildContext context, int i) {
-        if (i < 10) {
-          return Card(
-            elevation: 2.0,
-            child: ListTile(
-              leading: i%2 == 0 ? Icon(Icons.arrow_circle_up_rounded, size: 35, color: Colors.green,) : Icon(Icons.arrow_circle_down, size: 35, color: Colors.red,),
-              title: Text('Lorem Ipsum'),
-              subtitle: Text('Lorem Ipsum'),
-              trailing: i%2 == 0 ? Text('R\$10.00', style: TextStyle(fontSize: 20)) : Text('- R\$10.00', style: TextStyle(fontSize: 20),),
-            )
-          );
-        }
-      }),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: ListView.builder(itemBuilder: (BuildContext context, int i) {
+              if (i < 10) {
+                return Card(
+                  elevation: 2.0,
+                  child: ListTile(
+                    leading: i%2 == 0 ? Icon(Icons.arrow_circle_up_rounded, size: 35, color: Colors.green,) : Icon(Icons.arrow_circle_down, size: 35, color: Colors.red,),
+                    title: Text('Lorem Ipsum'),
+                    subtitle: Text('Lorem Ipsum'),
+                    trailing: i%2 == 0 ? Text('R\$10.00', style: TextStyle(fontSize: 20)) : Text('- R\$10.00', style: TextStyle(fontSize: 20),),
+                  )
+                );
+              }
+            }),
+          ),
+          Padding(
+            padding: EdgeInsetsGeometry.directional(start: 20, top: 5, end: 20, bottom: 5),
+            child: SizedBox(
+              height: 100,
+              child: Text("R\$ 0.00", style: TextStyle(fontSize: 50),),
+            ),
+          )
+      ],),
       floatingActionButton: FloatingActionButton(onPressed: (){
         Navigator.push(context, MaterialPageRoute(builder: (context){
           return TransactionFormPage();
