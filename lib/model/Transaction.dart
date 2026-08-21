72d0b7e25457cf951a153bc3a5dab10529e58a94
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2026-08-20 21:05:05 -0300

    feat: add transaction type in transaction model

--- lib/model/Transaction.dart
+++ lib/model/Transaction.dart
@@ -1,14 +1,17 @@
+import 'package:my_wallet/model/TransactionType.dart';
+
 class Transaction {
   String? _uuid;
   String _name;
   String? _description;
   double _value;
+  TransactionType _transactionType;
   DateTime? _createdAt;
   DateTime? _updatedAt;
 
-  Transaction(this._uuid, this._name, this._description,  this._value, this._createdAt, this._updatedAt);
+  Transaction(this._uuid, this._name, this._description,  this._value, this._transactionType, this._createdAt, this._updatedAt);
 
-  Transaction.create(this._name, this._description,  this._value, {DateTime? createdAt, DateTime? updatedAt}) {
+  Transaction.create(this._name, this._description, this._value, this._transactionType, {DateTime? createdAt, DateTime? updatedAt}) {
     this._createdAt = createdAt ?? DateTime.now();
     this._updatedAt = updatedAt ?? DateTime.now();
   }
