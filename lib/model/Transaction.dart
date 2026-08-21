30a9596e303293074b6d65fe6972c31d94d4c62e
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2026-08-20 22:47:17 -0300

    feat: creating getters and setters

--- lib/model/Transaction.dart
+++ lib/model/Transaction.dart
@@ -5,14 +5,36 @@ class Transaction {
   String _name;
   String? _description;
   double _value;
-  TransactionType _transactionType;
+  TransactionType _type;
   DateTime? _createdAt;
   DateTime? _updatedAt;
 
-  Transaction(this._uuid, this._name, this._description,  this._value, this._transactionType, this._createdAt, this._updatedAt);
+  Transaction(this._uuid, this._name, this._description,  this._value, this._type, this._createdAt, this._updatedAt);
 
-  Transaction.create(this._name, this._description, this._value, this._transactionType, {DateTime? createdAt, DateTime? updatedAt}) {
-    this._createdAt = createdAt ?? DateTime.now();
-    this._updatedAt = updatedAt ?? DateTime.now();
+  Transaction.create(this._name, this._description, this._value, this._type);
+
+  String? get uuid => _uuid;
+  String get name => _name;
+  String? get description => _description;
+  double get value => _value;
+  TransactionType get type => _type;
+  DateTime? get createdAt => _createdAt;
+  DateTime? get updatedAt => _updatedAt;
+
+  set name(String name){
+    _name = name;
   }
+
+  set description(String? description){
+    _description = description;
+  }
+
+  set value(double value){
+    _value = value;
+  }
+
+  set type(TransactionType type){
+    _type = type;
+  }
+
 }
